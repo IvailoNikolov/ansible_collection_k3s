ad9abafc615c18cf2744e553e746392612f37139
F K <fklieber@gmail.com>
2022-11-14 22:43:04 +0100

    init

--- plugins/action/github_get_release_name.py
+++ plugins/action/github_get_release_name.py
@@ -10,23 +10,36 @@ from ansible.errors import (
 import requests
 import yaml
 
+def validate_github_package(packages_info):
+        # retrieve package information
+
+        owner = packages_info.get('owner')
+        repo = packages_info.get('repo')
+        release_name = packages_info.get('release_name')
+
+        r_release_name = requests.get(f'https://api.github.com/repos/{owner}/{repo}/releases/tags/{release_name}')
+ 
+        if r_release_name.status_code != 200:
+            r_last_release_name = requests.get(f'https://api.github.com/repos/{owner}/{repo}/releases/latest')
+            r_last_release_name_yaml = yaml.safe_load(r_last_release_name.content)
+
+            release_name = r_last_release_name_yaml.get('tag_name')
+
+        return {"release_name": release_name}
+
 class ActionModule(ActionBase):
 
-    def validate_github_package(packages_info):
-    # retrieve package information
-    owner = packages_info.get('owner')
-    repo = packages_info.get('repo')
-    release_name = packages_info.get('release_name')
+    def _ensure_invocation(self, result):
+        # NOTE: adding invocation arguments here needs to be kept in sync with
+        # any no_log specified in the argument_spec in the module.
+        if "invocation" not in result:
+            if self._play_context.no_log:
+                result["invocation"] = "CENSORED: no_log is set"
+            else:
+                result["invocation"] = self._task.args.copy()
+                result["invocation"]["module_args"] = self._task.args.copy()
 
-    r_release_name = requests.get(f'https://api.github.com/repos/{owner}/{repo}/releases/tags/{release_name}')
-
-    if r_release_name.status_code != 200:
-        r_last_release_name = requests.get(f'https://api.github.com/repos/{owner}/{repo}/releases/latest')
-        r_last_release_name_yaml = yaml.safe_load(r_last_release_name.content)
-
-        release_name = r_last_release_name_yaml.get('tag_name')
-
-    return release_name
+        return result
     
     def run(self, tmp=None, task_vars=None):
         """handler for k8s options"""
@@ -36,17 +49,13 @@ class ActionModule(ActionBase):
         result = super(ActionModule, self).run(tmp, task_vars)
 
 
-        new_module_args = self._task.args
 
-        # helm install 
-        module_return = self.validate_github_package(
-            packages_info=new_module_args,
+        action_return = validate_github_package(
+            packages_info=self._task.args,
         )
 
         result.update(
-            self.validate_github_package(
-            packages_info=new_module_args,
-            )
+            action_return
         )
 
-        return result
+        return self._ensure_invocation(result)
